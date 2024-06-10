5eb82e6a2ab1a7dc5c0f3a6c4736e1954f9de590
Purvesh Panchal <137044431+purvesh-alite@users.noreply.github.com>
2024-06-10 19:34:54 +0530

    Create server-setup-part2.sh

---
+++ server-setup-part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+DOMAIN=$1 # Accept the domain name as the first argument
+
+# Install Certbot and configure SSL
+sudo apt-get install -y certbot python3-certbot-nginx
+sudo certbot --nginx -d $DOMAIN --non-interactive --agree-tos -m user@domain.com
+
+figlet "Nginx Setup Complete"
+export DOCKER_HUB_USERNAME=purveshpanchal
+export DOCKER_HUB_PASSWORD="Alite@123"
+# Log in to Docker Hub
+docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
+
+# Pull and run the application container
+docker pull purveshpanchal/tf-meteor:jenkins 
+docker run --name tf-meteor -dp 3000:3000 purveshpanchal/tf-meteor:jenkins
+
+figlet "Deployed Successfully!"
+echo "-----> $DOMAIN"
